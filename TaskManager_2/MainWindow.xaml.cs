2df166d53cb3d15911b01467b0eda150202a4299
Oleg <Oleg@DESKTOP-3130SV1>
2023-10-14 12:46:35 +0400

    Добавьте файлы проекта.

---
+++ TaskManager_2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TaskManager_2.DB_data;
+
+namespace TaskManager_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using (MainContext db = new MainContext())
+            {
+                var tasks = db.Tasks.ToList();
+                toDoTasks.ItemsSource = tasks;
+            }
+        }
+
+        private void testButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (MainContext db = new MainContext())
+            {
+                DB_data.Task task = new DB_data.Task();
+                db.Tasks.Add(task);
+            }
+        }
+    }
+}
